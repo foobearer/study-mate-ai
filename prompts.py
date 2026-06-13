f97536bfe9e9e59555fb698faaa96046af8eb245
foobearer <paragasjoycee1@gmail.com>
2026-06-13 14:40:27 +0400

    init: prompts and sample pdf file

---
+++ prompts.py
@@ -0,0 +1,11 @@
+PROMPT_RETRIEVING_SYS = '''You will receive a question from a student taking the Intro to AI course.
+    Answer the question using only the provided context.
+'''
+
+PROMPT_RETRIEVING_HUM = '''his is the question:
+{question}
+
+This is the context:
+{context}
+
+'''
